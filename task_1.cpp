81ab88a52de0d618feec9745f88e22db5f14bb1a
dmitrii-gbe <115346210+dmitrii-gbe@users.noreply.github.com>
2023-10-06 23:54:59 +0300

    Review 1

---
+++ task_1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+
+
+class EbookAccounter {
+    public:
+    EbookAccounter() : users_(std::vector<int>(100000)), number_of_users_(std::vector<int>(1000))
+    {
+    };
+    
+    void Read(int user_id, int page){
+        if (users_[user_id] == 0){
+            ++total_number_users_;
+        }
+        for (int i = users_[user_id] + 1; i <= page; ++i){
+            ++number_of_users_[i];
+        }
+        users_[user_id] = page;   
+    }
+    
+    void Cheer(int user_id){
+        if (users_[user_id] == 0){
+            std::cout << 0 << std::endl;
+        }
+        else if (users_[user_id] != 0 && total_number_users_ == 1) {
+            std::cout << 1 << std::endl;
+        }
+        else {
+            double number_of_lowers = total_number_users_ - number_of_users_[users_[user_id]];
+            double result = number_of_lowers / (total_number_users_ - 1);
+            std::cout << std::setprecision(6) << result << std::endl;
+        }
+
+    }
+    
+    private:
+    std::vector<int> users_;
+    std::vector<int> number_of_users_;
+    int total_number_users_ = 0;
+};
+
+
+
+int main(){
+    int counter;
+    EbookAccounter accounter;
+    std::cin >> counter;
+    while (counter != 0){
+        std::string request_type;
+        std::cin >> request_type;
+        if (request_type == "READ"){
+            int user = 0;
+            int page = 0;
+            std::cin >> user >> page;
+            accounter.Read(user, page);
+        }
+        if (request_type == "CHEER"){
+            int user = 0;
+            std::cin >> user;
+            accounter.Cheer(user);
+        }
+        --counter;
+    }
+    return 0;
+}
